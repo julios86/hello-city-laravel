cfe817213986ddcf7522cecf856c308285fc4843
julios86 <jules.fassinou@gmail.com>
2021-11-09 17:22:45 +0100

    Add about page and update links

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,14 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello julios</title>
+        <title>About Us | Hello julios</title>
 
 
 
     </head>
     <body >
-       <h1>Hello from Quebec!</h1>
-       <p> It's currently {{ date('h:i A')}}.</p>
+       <p>Built with &hearts; by Jules FASSINOU</p>
+       <p><a href="/"> Revenir à la page d'accueil</a></p>
        <footer>
            <p>&copy; Copyright {{ date('Y')}} &middot; <a href="/about-us">About Us</a></p>
        </footer>
--- resources/views/home.blade copy.php
+++ resources/views/home.blade copy.php
@@ -10,10 +10,10 @@
 
     </head>
     <body >
-       <h1>Hello from Quebec!</h1>
-       <p> It's currently {{ date('h:i A')}}.</p>
+       <p>Built with &hearts; by Jules FASSINOU</p>
+       <p><a href="/"> Revenir à la page d'accueil</a></p>
        <footer>
-           <p>&copy; Copyright {{ date('Y')}} &middot; <a href="/about-us">About Us</a></p>
+           <p>&copy; Copyright {{ date('Y')}} &middot; <a href="#">About Us</a></p>
        </footer>
     </body>
 </html>
