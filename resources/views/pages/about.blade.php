0bf79ef51c501f71c7c2a653fec01659ca1bf0d9
julios86 <jules.fassinou@gmail.com>
2021-11-10 15:52:45 +0100

    Add styling with tailwind css

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,8 +5,12 @@
 
 
 @section('content')
-        <img src="{{ asset('images/warren.png')}}" alt="warren">
-       <p>Built with &hearts; by Jules FASSINOU</p>
-       <p><a href="{{ route('home')}}"> Revenir à la page d'accueil</a></p>
+        <img src="{{ asset('images/warren.png')}}" alt="warren" class="my-12 rounded-full shadow-md">
+       <h1 class="mb-5 text-gray-700 ">
+           Built with <span class="text-pink-500">&hearts;</span> by Jules FASSINOU
+        </h1>
+       <p>
+           <a href="{{ route('home')}}" class="text-indigo-500 hover:text-indigo-600 underline"> Revenir à la page d'accueil</a>
+        </p>
 
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,8 +4,8 @@
 
 
 @section('content')
-    <img src="{{ asset('images/benin-flag.png')}}" alt="benin-flag">
-       <h1>Hello from Quebec!</h1>
-       <p> It's currently {{ date('h:i A')}}.</p>
+    <img src="{{ asset('images/benin-flag.png')}}" alt="benin-flag" class="mt-12 rounded shadow-md h-32">
+       <h1 class="mt-5 text-3xl sm:text-5xl font-semibold text-indigo-600">Hello from Benin!</h1>
+       <p class="mt-2 text-lg text-gray-800"> It's currently {{ date('h:i A')}}.</p>
 
 @endsection
