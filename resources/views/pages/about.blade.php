e3356be0580e303c6e612ab909d5ac0091ebb273
julios86 <jules.fassinou@gmail.com>
2021-11-10 09:23:44 +0100

    Add prope page titles

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,9 @@
 @extends('app')
 
+@section('title','About Us | Hello julios')
+
+
+
 @section('content')
        <p>Built with &hearts; by Jules FASSINOU</p>
        <p><a href="/"> Revenir à la page d'accueil</a></p>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,8 @@
 @extends('app')
 
+@section('title','Hello julios')
+
+
 @section('content')
        <h1>Hello from Quebec!</h1>
        <p> It's currently {{ date('h:i A')}}.</p>
